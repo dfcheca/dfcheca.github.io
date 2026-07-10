f45383eda378fad2c7f8c8d61fa4555dddfdf9a3
Daniel F. Checa <dcheca@unal.edu.co>
2026-07-09 19:24:45 -0500

    Updated llms.txt and search.js algorithm to index all the website

--- _includes/search-providers/default/search-data.js
+++ _includes/search-providers/default/search-data.js
@@ -11,5 +11,22 @@ window.TEXT_SEARCH_DATA = {
       }
       {%- endfor -%}
     ]
-  {%- endfor -%}
+  {%- endfor -%},
+  'pages': [
+    {%- assign _page_count = 0 -%}
+    {%- for _page in site.pages -%}
+      {%- assign _has_title = false -%}
+      {%- if _page.title -%}{%- assign _has_title = true -%}{%- endif -%}
+      {%- if _page.titles -%}{%- assign _has_title = true -%}{%- endif -%}
+      {%- unless _has_title == false or _page.title == 'Blog' or _page.title == 'Archive' -%}
+        {%- if _page_count > 0 -%},{%- endif -%}
+        {%- assign _page_count = _page_count | plus: 1 -%}
+        {
+          'title': {{ _page.title | default: 'Home' | jsonify }},
+          'url': {{ _page.url | jsonify }},
+          'content': {{ _page.content | jsonify | strip_newlines }}
+        }
+      {%- endunless -%}
+    {%- endfor -%}
+  ]
 };
